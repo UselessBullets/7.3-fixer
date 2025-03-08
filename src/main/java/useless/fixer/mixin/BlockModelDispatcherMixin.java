d85eea8b2d6303c685ac3b887ca9d7af8ea2f5f0
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2025-03-08 06:01:19 -0600

    Fixed chest bottom rotations

--- src/main/java/useless/fixer/mixin/BlockModelDispatcherMixin.java
+++ src/main/java/useless/fixer/mixin/BlockModelDispatcherMixin.java
@@ -7,7 +7,6 @@ import net.minecraft.client.util.dispatch.Dispatcher;
 import net.minecraft.core.block.Block;
 import net.minecraft.core.block.Blocks;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
