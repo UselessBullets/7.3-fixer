6b2d6f41d53d2f2646f5744b918281f18fc37cf0
bigsir24 <169484568+bigsir24@users.noreply.github.com>
2025-03-08 18:24:37 +0100

    Cache is now cleared after disconnecting, removed unnecessary code

--- src/main/java/useless/fixer/mixin/NetworkManagerMixin.java
+++ src/main/java/useless/fixer/mixin/NetworkManagerMixin.java
@@ -1,50 +1,24 @@
 package useless.fixer.mixin;
 
 import net.minecraft.core.net.NetworkManager;
+import net.minecraft.core.net.packet.PacketBlockRegionUpdate;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import useless.fixer.interfaces.IBuffer;
 
 @Mixin(value = NetworkManager.class, remap = false)
 public abstract class NetworkManagerMixin {
-	/*
-	@Mutable
-	@Shadow
-	@Final
-	private List<Packet> incoming;
-
-	@Shadow
-	@Final
-	private List<Packet> outgoing;
-
-	@Inject(method = "processReadPackets", at = @At("HEAD"))
-	public void size(CallbackInfo ci){
-		Map<String, Integer> map = new HashMap<>();
-
-		int max = -1;
-		String maxName = "";
-		for (int i = 0; i < this.incoming.size(); i++) {
-			Packet packet = this.incoming.get(i);
-
-			String name = this.incoming.get(i).getClass().getSimpleName();
-			int val = map.getOrDefault(name, 0) + 1;
-			map.put(name, val);
-
-			if(val > max){
-				max = val;
-				maxName = name;
-			}
-		}
-		StringBuilder out = new StringBuilder();
-		for (Map.Entry<String, Integer> e : map.entrySet()){
-			out.append(e.getKey()).append(": ").append(e.getValue()).append(" | ");
-		}
-		System.out.println("\t" + this.incoming.size() + "\t" + this.outgoing.size() + "\t | " + out);
-	}
-	 */
-
 	@ModifyConstant(method = "processReadPackets", constant = @Constant(intValue = 100))
 	private int mod(int constant){
 		return 1000;
+	}
+
+	@Inject(method = "networkShutdown", at = @At("HEAD"))
+	public void clearPacketCache(String reason, Object[] reasonObjects, CallbackInfo ci) {
+		((IBuffer)new PacketBlockRegionUpdate()).btafix$destroyCache();
 	}
 }
